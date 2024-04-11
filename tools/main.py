aaac1fb663043743f234cbf9a72e785412a19df4
0x4248 <60709927+0x4248@users.noreply.github.com>
2024-04-11 11:56:28 +0100

    Username update
    Signed-off-by: 0x4248 <60709927+0x4248@users.noreply.github.com>

--- tools/main.py
+++ tools/main.py
@@ -1,8 +1,8 @@
 # OSM Images
 # A easy way to use Github Raw to host images for OSM
-# Github: https://www.github.com/lewisevans2007/OSM_Images
+# Github: https://www.github.com/0x4248/OSM_Images
 # Licence: GNU General Public License v3.0
-# By: Lewis Evans
+# By: 0x4248
 
 """
 README
